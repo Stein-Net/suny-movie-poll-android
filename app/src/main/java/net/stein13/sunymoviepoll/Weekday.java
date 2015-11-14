67a27ce4e81dc4853fb2fe4ae9a98586ea38979c
pendent20 <pendent20@yahoo.com>
2015-11-13 16:32:04 -0800

    Revert "Working with Weekday.java and URL Connection"
    This reverts commit 53c77384ac72ce42c48e0a41095dc6dc8f7ba295.

--- app/src/main/java/net/stein13/sunymoviepoll/Weekday.java
+++ app/src/main/java/net/stein13/sunymoviepoll/Weekday.java
@@ -1,14 +1,9 @@
 package net.stein13.sunymoviepoll;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import android.app.DownloadManager;
+
+import org.json.JSONObject;
 
 /**
  * Created by gregory on 10/24/15.
@@ -17,59 +12,12 @@ public class Weekday {
     private String day;
     private String movies[] = {"movie1", "movie2", "movie3", "movie4", "movie5", "movie6"};
     private int votes[] = {0,0,0,0,0,0};
-    String url = "";
     public Weekday(String day1){
         day = day1;
-        url = "https://suny-movie-poll.firebaseio.com/movie-list/" + day + ".json";
+        String url = "https://suny-movie-poll.firebaseio.com/movie-list/" + day + ".json";
        
     }
 
-    public String getJSON(int timeout) {
-        HttpURLConnection c = null;
-        try {
-            URL u = new URL(url);
-            c = (HttpURLConnection) u.openConnection();
-            c.setRequestMethod("GET");
-            c.setRequestProperty("Content-length", "0");
-            c.setUseCaches(false);
-            c.setAllowUserInteraction(false);
-            c.setConnectTimeout(timeout);
-            c.setReadTimeout(timeout);
-            c.connect();
-            int status = c.getResponseCode();
-
-            switch (status) {
-                case 200:
-                case 201:
-                    BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
-                    StringBuilder sb = new StringBuilder();
-                    String line;
-                    while ((line = br.readLine()) != null) {
-                        sb.append(line+"\n");
-                    }
-                    br.close();
-                    return sb.toString();
-            }
-
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            if (c != null) {
-                try {
-                    c.disconnect();
-                } catch (Exception ex) {
-                    Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        }
-        return null;
-    }
-    public void setData() {
-        String data = getJSON();
-
-    }
 
 
 }
